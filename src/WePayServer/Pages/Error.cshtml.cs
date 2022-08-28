a8d964383c58303034b7577dd44848ff129e5e27
he55 <40764272+he55@users.noreply.github.com>
2022-08-28 18:24:59 +0800

    update project

--- src/WePayServer/Pages/Error.cshtml.cs
+++ src/WePayServer/Pages/Error.cshtml.cs
@@ -1,14 +1,15 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace WePayServer.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; } = string.Empty;
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
--- src/WePayServer/Pages/Index.cshtml.cs
+++ src/WePayServer/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
