5dde75e72e1865b7b04ccb1871b1c8b0a365f8f7
he55 <40764272+he55@users.noreply.github.com>
2023-03-05 00:31:41 +0800

    1

--- src/WePayServer/Pages/Order/Details2.cshtml.cs
+++ src/WePayServer/Pages/Order/Details2.cshtml.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace WePayServer.Pages.Order
 {
-    public class IndexModel : PageModel
+    public class Details2Model : PageModel
     {
         private readonly WePayContext _context;
 
-        public IndexModel(WePayContext context)
+        public Details2Model(WePayContext context)
         {
             _context = context;
         }
