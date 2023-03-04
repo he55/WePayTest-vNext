491a1fbcde0b7c1dcd0ce4c90c93febc79e44ce4
he55 <40764272+he55@users.noreply.github.com>
2023-03-05 00:24:16 +0800

    1

---
+++ src/WePayServer/Pages/Order/aa/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WePayServer.Pages.Order
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WePayContext _context;
+
+        public IndexModel(WePayContext context)
+        {
+            _context = context;
+        }
+
+        public IList<WePayOrder> Orders { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Orders = await _context.WePayOrders.ToListAsync();
+        }
+    }
+}
